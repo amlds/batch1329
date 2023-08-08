b2c3382a08ec6a32ca85623d05e4aa7a76abc5e0
Alexy MARTINET <45981954+amlds@users.noreply.github.com>
2023-08-08 16:31:28 +0200

    update

--- ajax-lecture-boilerplate/javascript/application.js
+++ ajax-lecture-boilerplate/javascript/application.js
@@ -1,1 +1,37 @@
+import Swal from 'sweetalert2';
+
 console.log("Hello from JavaScript!");
+const urlFakeApi = "https://reqres.in/api/register"
+
+// const url = "https://api.github.com/users/dhh"
+
+// fetch(url) // Make the HTTP request
+//   .then(response => response.json()) // Wait for the response and parse it as JSON
+//   .then((data) => {
+//     console.log(data); // Wait for parsing, allowing us to manipulate the data
+//   })
+
+const form = document.querySelector('.form');
+
+console.log(form)
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  loginFakeApi(event.target[0].value, event.target[1].value)
+})
+
+const loginFakeApi = (email, password) => {
+  fetch(urlFakeApi, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({"email": email, "password": password})
+  })
+  .then((response) => {
+    if (response.status === 200) {
+      Swal.fire({title: 'Success', text: 'You are connected', icon: 'success'})
+    } else {
+      Swal.fire({title: 'Error!', text: 'Oups! Something went wrong', icon: 'error'})
+    }
+  })
+  .then(data => console.log(data))
+}
--- http-and-api-lecture-boilerplate/lib/index.js
+++ http-and-api-lecture-boilerplate/lib/index.js
@@ -1,2 +1,17 @@
 // TODO - Fetch an activity with the Bored API - Let's psuedocode!
 const url = 'https://www.boredapi.com/api/activity/'
+
+const button = document.querySelector('.btn');
+const activity = document.querySelector('#activity')
+
+const putInformation = (data, balise) => {
+  balise.innerText = data
+}
+
+button.addEventListener('click', () => {
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      putInformation(data.activity, activity)
+    });
+})
